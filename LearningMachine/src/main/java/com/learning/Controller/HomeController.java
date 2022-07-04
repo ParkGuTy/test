27c825d2e024a3f21e02a7e35fc9d40d1d3b1882
Hijineee <yiheejin92@nate.com>
2022-07-04 18:15:53 +0900

    2022.07.04 header, footer, login form 작성

---
+++ LearningMachine/src/main/java/com/learning/Controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.learning.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(value = "/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping(value = "/contact")
+	public String contact() {
+		return "contact";
+	}
+	
+	@GetMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+	
+}
